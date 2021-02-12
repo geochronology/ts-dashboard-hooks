e5fc19e4d8ed9259965103cbe34cbe39e3abd0b5
geochronology <54387644+geochronology@users.noreply.github.com>
2021-02-11 16:12:34 -0800

    added login component w redirect

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,48 @@
-import React, { Component } from 'react'
+import axios from 'axios'
+import React, { SyntheticEvent, useState } from 'react'
+import { Redirect } from 'react-router-dom'
 
-class Login extends Component {
-  render() {
-    return (
-      <div>
-        Login
-      </div>
+const Login = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [redirect, setRedirect] = useState(false)
+
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault()
+
+    const { data } = await axios.post('http://localhost:8000/api/login', {
+      email,
+      password
+    }, { withCredentials: true } // required for getting cookie to login
     )
+
+    setRedirect(true)
   }
+
+  if (redirect) {
+    return (<Redirect to={'/'} />)
+  }
+
+  return (
+
+    <main className="form-signin">
+      <form onSubmit={submit}>
+
+        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <input type="email" className="form-control" placeholder="Email address" required
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <input type="password" className="form-control" placeholder="Password" required
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <button className="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+      </form>
+    </main>
+  )
 }
 
 export default Login
