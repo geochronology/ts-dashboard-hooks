30b4db58347667002a6ce1c0057cd92db6c4454e
geochronology <54387644+geochronology@users.noreply.github.com>
2021-02-12 15:09:13 -0800

    added pagination

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -6,17 +6,31 @@ import { User } from '../../models/user';
 const Users = () => {
 
   const [users, setUsers] = useState([])
+  const [page, setPage] = useState(1)
+  const [lastPage, setLastPage] = useState(0)
 
   useEffect(() => {
     (
       async () => {
-        const { data } = await axios.get("users")
+        const { data } = await axios.get(`users?page=${page}`)
 
         setUsers(data.data)
+        setLastPage(data.meta.last_page)
       }
     )()
+  }, [page])
 
-  }, [])
+  const next = () => {
+    if (page < lastPage) {
+      setPage(page + 1)
+    }
+  }
+
+  const prev = () => {
+    if (page >= 1) {
+      setPage(page - 1)
+    }
+  }
 
   return (
     <Wrapper>
@@ -50,6 +64,18 @@ const Users = () => {
           </tbody>
         </table>
       </div>
+
+      <nav>
+        <ul className="pagination">
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={prev}>Previous</a>
+          </li>
+          <li className="page-item">
+            <a href="#" className="page-link" onClick={next}>Next</a>
+          </li>
+        </ul>
+      </nav>
+
     </Wrapper>
   );
 
