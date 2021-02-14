f28040c8f8d618f16a6f9cdc32b0db3acfb4c74e
geochronology <54387644+geochronology@users.noreply.github.com>
2021-02-13 17:00:18 -0800

    added ability to update users

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Users from './pages/users/Users';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import { UserCreate } from './pages/users/UserCreate';
+import { UserEdit } from './pages/users/UserEdit';
 
 function App() {
   return (
@@ -14,9 +16,11 @@ function App() {
 
       <BrowserRouter>
         <Route path={'/'} exact component={Dashboard} />
-        <Route path={'/users'} component={Users} />
         <Route path={'/register'} component={Register} />
         <Route path={'/login'} component={Login} />
+        <Route path={'/users'} exact component={Users} />
+        <Route path={'/users/create'} component={UserCreate} />
+        <Route path={'/users/:id/edit'} component={UserEdit} />
       </BrowserRouter>
 
     </div >
